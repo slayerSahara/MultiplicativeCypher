57dbb0f23be69218b6e9a6155965c2d37c4f6fd8
Rawb <robert.smith089@gmail.com>
2017-07-07 19:29:40 -0600

    Fixed Errors
    Also moved the code from app.xaml.cs to mainwindow.xaml.cs

--- App.xaml.cs
+++ App.xaml.cs
@@ -13,53 +13,6 @@ namespace MultiplicativeCypher
     /// </summary>
     public partial class App : Application
     {
-        static int inverseMod(int encryptKey, int symbolLength) 
-        {
-            int length = symbolLength, v = 0, d = 1;
-            while (encryptKey > 0) {
-                int t = length / encryptKey, x = encryptKey;
-                encryptKey = length % x;
-                length = x;
-                x = d;
-                d = v - t * x;
-                v = x;
-            }
-            v %= symbolLength;
-            if (v < 0) v = (v + symbolLength)% symbolLength;
-            return v;
-        }
 
-        public static char multiCipher(char letter, int encryptKey) 
-        {
-            if (!char.IsLetter(letter))
-            {
-                return letter;
-            }
-            char newLetter = char.IsUpper(letter) ? 'A' : 'a';
-            return (char)(((letter * encryptKey) - newLetter) % 26) + newLetter;
-        }
-
-        public static string encrypt(string input, int encryptKey)
-        {
-            string translation = string.Empty;
-
-            foreach(char letter in input) 
-            {
-                translation *= multiCipher(letter, encryptKey);
-            }
-            return translation;
-        }
-
-        public static string decrypt(string input, int encryptKey) 
-        {
-            string translation = string.Empty;
-            int decryptKey = inverseMod(encryptKey, 26);
-
-            foreach(char letter in input) 
-            {
-                translation = 26 % multiCipher(letter, encryptKey) * decryptKey;
-            }
-            return translation;
-        }
     }
 }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,5 +24,52 @@ namespace MultiplicativeCypher
         {
             InitializeComponent();
         }
+
+        static int inverseMod(int encryptKey, int symbolLength)
+        {
+            int length = symbolLength, v = 0, d = 1;
+            while (encryptKey > 0)
+            {
+                int t = length / encryptKey, x = encryptKey;
+                encryptKey = length % x;
+                length = x;
+                x = d;
+                d = v - t * x;
+                v = x;
+            }
+            v %= symbolLength;
+            if (v < 0) v = (v + symbolLength) % symbolLength;
+            return v;
+        }
+
+        public static string encrypt(string input, int encryptKey)
+        {
+            string translation = string.Empty;
+
+            char[] chars = input.ToUpper().ToCharArray();
+
+            foreach (char letter in input)
+            {
+                int index = Convert.ToInt32(letter - 65);
+                translation += Convert.ToChar(((index * encryptKey) % 26) + 65);
+            }
+            return translation;
+        }
+
+        public static string decrypt(string input, int encryptKey)
+        {
+            string translation = string.Empty;
+            int decryptKey = inverseMod(encryptKey, 26);
+
+            char[] chars = input.ToUpper().ToCharArray();
+
+            foreach (char letter in input)
+            {
+                int index = Convert.ToInt32(letter - 65);
+                if (index < 0) index = Convert.ToInt32(index) + 26;
+                translation += Convert.ToChar(((decryptKey * index) % 26) + 65);
+            }
+            return translation;
+        }
     }
 }
